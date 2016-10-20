42bc056659ed328f6754c75c48cac7c00a119746
peter3110 <peter3110>
2016-10-20 18:12:26 -0300

    busqudas

---
+++ busquedas.sql
@@ -0,0 +1,21 @@
+-- Busquedas
+
+-- Datos de las personas que fueron sospechosas
+
+-- Direcciones donde convivieron personas sospechosas de diferentes casos
+
+-- Oficiales que participaron en la cadena de custodia de evidencias para más de un caso
+
+-- La sucesión de eventos de personas involucradas en un caso
+
+-- Un ranking de oficiales exitosos, es decir los que cerraron mayor cantidad de casos (resueltos)
+
+-- Las ubicaciones de todas las evidencias de un caso
+
+-- La lista de oficiales involucrados en un caso
+
+-- Las categorías de casos ordenadas por cantidad de casos
+
+-- Todos los testimonios de un caso dado
+
+--Para una categoría en particular listar, para cada uno de los casos, los testimonios asociados
